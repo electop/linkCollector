2b8e1cb974d115d6acf5eea07c94769a0c040e33
Juhwan Yoo <electopx@gmail.com>
2018-06-02 02:29:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,6 @@ http.client.HTTPConnection._http_vsn_str = 'HTTP/1.0'
 start = time.time()
 rnum, dnum, maxnum = 0, 0, 0
 maxthreadsnum = 15	# If the performance of your PC is low, please adjust this value to 5 or less.
-maxDepth = 2
 cu, du, url, prefix, path = '', '', '', '', ''
 rdf = DataFrame(columns=('link', 'code'))
 df = DataFrame(columns=('link', 'visited'))
@@ -77,16 +76,14 @@ def getCode(tu):
         status = True
     except HTTPError as e:
         code = str(e.code)
-        print('\n[ERR] HTTP Error: The server couldn\'t fulfill the request to\n%s' %tu)
+        print('\n[ERR] HTTP Error: The server couldn\'t fulfill the request to\n%s\n+ %s' %(tu, code))
     except URLError as e:
         code = e.reason
-        print('\n[ERR] URL ERror: We failed to reach in\n%s\n+ %s' %(tu, code))
+        print('\n[ERR] URL Error: We failed to reach in\n%s\n+ %s' %(tu, code))
 
-    rows = [tu, code]
-    rdf.loc[rnum] = rows
+    rows = {'link': tu, 'code': code}
+    rdf = rdf.append(rows, ignore_index=True)
     rnum = rnum + 1
-    #rows = {'link':tu, 'code':code}
-    #rdf = rdf.append(rows, ignore_index=True)
     counts = len(rdf)
 
     end = time.time()
@@ -105,7 +102,9 @@ def getCode(tu):
 def getLink(tu, visited):
     global df	# df: data frame
     global cu, maxnum, dnum, maxDepth	# maxnum: maximum # of data frame
-    excludedfiles = '.ico.png.jpg.jpeg.gif.pdf.bmp.tif.svg.pic.rle.psd.pdd.raw.ai.eps.iff.fpx.frm.pcx.pct.pxr.sct.tga.vda.icb.vst'
+    #excludedfiles = '.ico.png.jpg.jpeg.gif.pdf.bmp.tif.svg.pic.rle.psd.pdd.raw.ai.eps.iff.fpx.frm.pcx.pct.pxr.sct.tga.vda.icb.vst'
+    # The most common file types and file extensions
+    excludedfiles = '.aif.cda.mid.mp3.mpa.ogg.wav.wma.wpl.7z.arj.deb.pkg.rar.rpm.tar.z.zip.bin.dmg.iso.toa.vcd.csv.dat.db.log.mdb.sav.sql.tar.xml.apk.bat.bin.cgi.com.exe.gad.jar.py.wsf.fnt.fon.otf.ttf.ai.bmp.gif.ico.jpe.png.ps.psd.svg.tif.asp.cer.cfm.cgi.js.jsp.par.php.py.rss.key.odp.pps.ppt.ppt.c.cla.cpp.cs.h.jav.sh.swi.vb.ods.xlr.xls.xls.bak.cab.cfg.cpl.cur.dll.dmp.drv.icn.ico.ini.lnk.msi.sys.tmp.3g2.3gp.avi.flv.h26.m4v.mkv.mov.mp4.mpg.rm.swf.vob.wmv.doc.odt.pdf.rtf.tex.txt.wks.wpd'
 
     if visited:
         #print ('[OK] It\'s already visited to the URL below and skip.\n%s\n' %tu)
@@ -114,8 +113,8 @@ def getLink(tu, visited):
     if len(df.loc[df['link'] == tu]) > 0:
         df.loc[df['link'] == tu, 'visited'] = True
     else:
-        rows = [tu, True]
-        df.loc[dnum] = rows
+        rows = {'link': tu, 'visited': True}
+        df = df.append(rows, ignore_index=True)
         dnum = dnum + 1
 
     (status, html) = getCode(tu)
@@ -126,7 +125,10 @@ def getLink(tu, visited):
     tokens = tu.split('/')
     lasttoken = tokens[-1]
 
-    if lasttoken.find('#') >= 0 or lasttoken.find('?') >= 0 or lasttoken.find('%') >= 0 or excludedfiles.find(lasttoken[-4:]) >= 0:
+    if tu.find('?') >= 0:
+        print ('+ This URL is skipped because it`s not the target of the getLink().')
+        return False
+    elif lasttoken.find('#') >= 0 or lasttoken.find('%') >= 0 or excludedfiles.find(lasttoken[-4:]) >= 0:
         print ('+ This "%s" is skipped because it`s not the target of the getLink().' %lasttoken)
         return False
     else:
@@ -146,11 +148,9 @@ def getLink(tu, visited):
             if nl.find(cu) >= 0 and nl != tu:
                 maxnum = maxnum + 1
                 if len(df.loc[df['link'] == nl]) == 0:
-                    rows = [nl, False]
-                    df.loc[dnum] = rows
+                    rows = {'link': nl, 'visited': False}
+                    df = df.append(rows, ignore_index=True)
                     dnum = dnum + 1
-                    #rows = {'parent':tu, 'link':nl, 'visited':False}
-                    #df = df.append(rows, ignore_index=True)
                     print ('+ Adding rows(%d):\n%s'%(dnum, rows))
 
         return True
